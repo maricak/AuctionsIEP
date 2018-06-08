8197f0bf4e8c94bd917d171e852b58ed6151a454
Marija Kostic <marijakostic25@gmail.com>
2018-06-08 13:30:47 +0200

    Add project files.

---
+++ Auction.Data/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Auction.Data.Startup))]
+namespace Auction.Data
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
