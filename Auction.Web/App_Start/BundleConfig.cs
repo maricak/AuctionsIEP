185103ba066b6eeced996485fc4e635eb55b5c6a
Marija Kostic <marijakostic25@gmail.com>
2018-06-10 20:29:45 +0200

    Changed design from bootstrap to lumen bootstrap

--- Auction.Web/App_Start/BundleConfig.cs
+++ Auction.Web/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Auction.Web
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css"/*,
+                      "~/Content/lumen-bootstrap.css"/*,
                       "~/Content/site.css"*/));
         }
     }
